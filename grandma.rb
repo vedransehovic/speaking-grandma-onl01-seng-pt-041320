f1e1128a88995911f71ab2771269c5059a6a20ba
Vedran Sehovic <vedran.sehovic@gmail.com>
2020-04-15 15:19:16 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@
 isay="HI! YOU GRANDMA!" #my variable
 
 def speak_to_grandma(phrase)
-  if phrase == ""
+  if phrase == "I LOVE YOU GRANDMA!"
     return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
