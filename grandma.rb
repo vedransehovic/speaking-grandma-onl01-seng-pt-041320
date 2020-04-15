5bcfcaa8f08d12587b3476c309d848a9205c7ad9
Vedran Sehovic <vedran.sehovic@gmail.com>
2020-04-15 15:23:22 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -18,7 +18,7 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase
-    
+    puts "NO, NOT SINCE 1938!"
     return "NO, NOT SINCE 1938!"
   else 
     puts "HUH?! SPEAK UP, SONNY!"
